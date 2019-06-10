ebb4d6107aae2f0eeefc74f39c10af95c54b3999
S.H.Kim <49853036+skok1025@users.noreply.github.com>
2019-06-10 17:34:51 +0900

    Create shutdown.sh

---
+++ cafe24/tomcat-cafe24/bin/shutdown.sh
@@ -0,0 +1,6 @@
+  #!/usr/bin/env bash
+
+  export CATALINA_BASE=/usr/local/cafe24/tomcat-cafe24
+  export JAVA_OPTS="-Djava.awt.headless=true -server -Xms512m -Xmx1024m -XX:NewSize=256m -XX:MaxNewSize=256m -XX:PermSize=256m -XX:MaxPermSize=512m -XX:+DisableExplicitGC"
+  export CATALINA_OPTS="-Denv=product -Denv.servername=cafe24"
+  $CATALINA_HOME/bin/catalina.sh stop
